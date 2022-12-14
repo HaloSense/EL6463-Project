f1a3f8a39bbc9c9e0379e1d586c3486fb8e15715
chandrakirannarala <chandrakirannarala@gmail.com>
2022-12-14 04:30:39 -0500

    Updated Immxt_tb changes

--- btm_layer_design/btm_layer_design.srcs/sources_1/new/immext.v
+++ btm_layer_design/btm_layer_design.srcs/sources_1/new/immext.v
@@ -22,55 +22,55 @@
 
 module immext(
     input  [31:0]instr_in,
-    output [31:0] imm_ext
+    output [31:0] instr_out
     );
     reg [31:0]imm_temp;
-
-    reg sign_ext_imm;
+    wire [6:0] opcode;
+    wire [4:0] rs2_temp;
     assign opcode   = instr_in[6:0];
-    assign rs2_addr = instr_in[24:20];
+    assign rs2_temp = instr_in[24:20];
    
-    localparam [6:0]R_TYPE  = 7'b0110011, // op: 000
-                I_TYPE  = 7'b0010011, // op: 001
-                S_TYPE   = 7'b0100011, // op: 010
-                L_TYPE    = 7'b0000011, // op: 011
-                B_TYPE  = 7'b1100011,   // op: 100
-                JALR    = 7'b1100111,   // op: 101
-                JAL     = 7'b1101111,   // op  101
-                AUIPC   = 7'b0010111,   // op: 110
-                LUI     = 7'b0110111;   // op: 110
+//    localparam [6:0]R_TYPE  = 7'b0110011, // op: 000
+//                I_TYPE  = 7'b0010011, // op: 001
+//                S_TYPE   = 7'b0100011, // op: 010
+//                L_TYPE    = 7'b0000011, // op: 011
+//                B_TYPE  = 7'b1100011,   // op: 100
+//                JALR    = 7'b1100111,   // op: 101
+//                JAL     = 7'b1101111,   // op  101
+//                AUIPC   = 7'b0010111,   // op: 110
+//                LUI     = 7'b0110111;   // op: 110
     
 
 
    
-   always @(imm_in,opcode)
+   always @(instr_in)
         case (opcode)
-        LUI: 
+        7'b0110111: 
             imm_temp = {instr_in[31:12], 12'b0};//LUI
-        AUIPC: 
+        7'b0010111: 
             imm_temp = {instr_in[31:12], 12'b0};//AUIPC
-        JAL: 
+        7'b1101111: 
             imm_temp = {{12{instr_in[31]}}, instr_in[19:12], instr_in[20], instr_in[30:21], 1'b0};//JAL
-        JALR: 
+        7'b1100111: 
             imm_temp = {{20{instr_in[31]}}, instr_in[31:20]};//JALR
-        B_TYPE: 
+        7'b1100011: 
             imm_temp = {{20{instr_in[31]}}, instr_in[7], instr_in[30:25], instr_in[11:8], 1'b0};//BEQ, BNE, BLT, BGE, BLUT, BGEU
-        L_TYPE: 
+        7'b0000011: 
             imm_temp = {{20{instr_in[31]}}, instr_in[31:20]};//LB, LH, LW, LBU, LHU
-        S_TYPE: 
+        7'b0100011: 
             imm_temp = {{20{instr_in[31]}}, instr_in[31:25], instr_in[11:7]};//SB, SH, SW
-        I_TYPE: 
+        7'b0010011: 
             begin 
-            if((instr_in[31:25]==7'b0100000&&instr_in[14:12]==3'b101)||(instr_in[14:12]==3'b001)||instruction[14:12]==3'b101)
-                imm_temp = {{27{rs2_addr[4]}}, rs2_add};
+            if((instr_in[31:25]==7'b0100000&&instr_in[14:12]==3'b101)||(instr_in[14:12]==3'b001)||instr_in[14:12]==3'b101)
+                imm_temp = {{27{rs2_temp[4]}}, rs2_temp};
             else
                 imm_temp = {{20{instr_in[31]}}, instr_in[31:20]};//ADDI, SLTI, SLTIU, XORI, ORI, ANDI
             end
         default
-            imm_out <= {32'b0};
+            imm_temp <= {32'b0};
 
 endcase
 
    
-   assign imm_ext = imm_temp;
+   assign instr_out = imm_temp;
 endmodule
